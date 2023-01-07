46c687d8dbdf0320cb4b5d62e3d51131ad50f2fc
etwinn <emily_winn@brown.edu>
2023-01-06 19:40:39 -0500

    MCMC functions and tests

--- R/file_read_write.R
+++ R/file_read_write.R
@@ -1,5 +1,17 @@
 #read and write OFF, OFF4, and text files for alpha shapes
 
+#' Read OFF File
+#' 
+#' This is a function to read OFF files for triangular meshes into the form 
+#' that is required to use other functions in the package.
+#'
+#' @param file_name path and name of file to be read
+#'
+#' @return complex_info list object containing two components, "Vertices" which 
+#'        holds the vertex coordinates and "cmplx" which holds the complex list
+#'        object.
+#' @export
+#'
 readOFF <- function(file_name){
   off=Rvcg::vcgImport(file_name,silent = TRUE)
   vertices=as.matrix(t(off$vb)[,1:3])
@@ -9,9 +21,18 @@ readOFF <- function(file_name){
   complex = append(as.list(data.frame(t(edges[,1:2]))), as.list(data.frame(t(faces))))
   complex = append(as.list(1:n), complex)
   complex_info = list("Vertices"=vertices, "cmplx"= complex)
-  return(vertices, complex)
+  return(complex_info)
 }
 
+#' Write Alpha Text file
+#'
+#' @param ashape alpha shape object, can be 2D or 3D alpha shape
+#' @param file_name path and name of file to create and write text to
+#'
+#' @return does not return anything; writes file that can be read back to R via
+#'         read_alpha_txt
+#' @export
+#'
 write_alpha_txt <- function(ashape, file_name){
   vertices = ashape$x
   my_alpha = ashape$alpha
@@ -23,6 +44,14 @@ write_alpha_txt <- function(ashape, file_name){
   writeLines(testtext, file_name)
 }
 
+#' Read alpha text file
+#'
+#' @param file_name name and path of file to be read. File is of format output 
+#'                  by write_alpha_txt function
+#'
+#' @return alpha shape object
+#' @export
+#'
 read_alpha_txt <- function(file_name){
   input_file = readLines(file_name)
   if(tolower(input_file[1])!="alpha"){
