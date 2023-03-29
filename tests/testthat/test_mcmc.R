3705064161aa0e7f1211da130138a6c7d4402166
etwinn <emily_winn@brown.edu>
2023-03-29 13:46:37 -0400

    updating generating functions

--- tests/testthat/test_mcmc.R
+++ tests/testthat/test_mcmc.R
@@ -9,8 +9,8 @@ test_that("mcmc errors", {
   expect_error(generate_ashape3d(points2, N, tau))
   expect_error(generate_ashape2d(points2, N, tau, afixed=FALSE, sig=-1))
   expect_error(generate_ashape3d(points3, N, tau, afixed=FALSE, sig=-1))
-  expect_error(generate_ashape2d(points2, N=-1, tau))
-  expect_error(generate_ashape3d(points3, N=-1, tau))
+  expect_error(generate_ashape2d(points2, J=-1, tau))
+  expect_error(generate_ashape3d(points3, J=-1, tau))
   expect_error(generate_ashape2d(points2, N, tau=-0.7))
   expect_error(generate_ashape3d(points3, N, tau=-0.7))
   expect_error(generate_ashape3d(points3, N, tau=0.0001))
@@ -22,20 +22,20 @@ test_that("mcmc errors", {
 test_that("2D mcmc smooth runs", {
   #given
   tau=1
-  N=2
+  J=2
   points2 = cbind(stats::runif(20), stats::runif(20))
   #then
-  expect_no_error(generate_ashape2d(points2, N, tau))
+  expect_no_error(generate_ashape2d(points2, J, tau))
 })
 
 test_that("3D mcmc smooth runs", {
   #given
   set.seed(201727)
   tau=1
-  N=2
+  J=2
   points3 = cbind(stats::runif(500,0.5,1), stats::runif(500,0.5,1),
                   stats::runif(500,0.5,1))
   #then
-  expect_no_error(generate_ashape3d(points3, N, tau))
+  expect_no_error(generate_ashape3d(points3, J, tau))
 })
 
